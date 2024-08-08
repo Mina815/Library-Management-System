f8d189abb51ef2db276dfe2e01bec79e905a2e48
Mina Latif <minalatifl815@gmail.com>
2024-08-08 18:27:36 +0300

    BorrowingRecordController..

--- src/main/java/com/task/library_management_system/controllers/BorrowingRecordController.java
+++ src/main/java/com/task/library_management_system/controllers/BorrowingRecordController.java
@@ -1,2 +1,29 @@
-package com.task.library_management_system.controllers;public class BorrowingRecordController {
+package com.task.library_management_system.controllers;
+
+import com.task.library_management_system.dtos.BorrowingRecordDto;
+import com.task.library_management_system.services.BorrowingRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class BorrowingRecordController {
+    @Autowired
+    private BorrowingRecordService borrowingRecordService;
+
+    @PostMapping("/borrow/{bookId}/patron/{patronId}")
+    public ResponseEntity<BorrowingRecordDto> borrowBook(
+            @PathVariable Integer bookId,
+            @PathVariable Integer patronId) {
+        BorrowingRecordDto borrowingRecordDTO = borrowingRecordService.borrowBook(bookId, patronId);
+        return ResponseEntity.ok(borrowingRecordDTO);
+    }
+    @PutMapping("/return/{bookId}/patron/{patronId}")
+    public ResponseEntity<BorrowingRecordDto> returnBook(
+            @PathVariable Integer bookId,
+            @PathVariable Integer patronId) {
+        BorrowingRecordDto borrowingRecordDTO = borrowingRecordService.returnBook(bookId, patronId);
+        return ResponseEntity.ok(borrowingRecordDTO);
+    }
 }
--- src/main/java/com/task/library_management_system/repos/BorrowingRecordRepo.java
+++ src/main/java/com/task/library_management_system/repos/BorrowingRecordRepo.java
@@ -3,5 +3,8 @@ package com.task.library_management_system.repos;
 import com.task.library_management_system.entities.BorrowingRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BorrowingRecordRepo extends JpaRepository<BorrowingRecord, Integer> {
+    Optional<BorrowingRecord> findByBookIdAndPatronIdAndReturnDateIsNull(Integer bookId, Integer patronId);
 }
